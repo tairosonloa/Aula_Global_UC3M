82b4b726bc54b0660a648e6a2cf55638fefc5602
Aitor Alonso <aalonso@aalonso.eu>
2019-10-30 13:09:26 +0100

    New version. Now acts as a crawler and not longer requires access to the moodle API. Resolves #3 #4

--- Readme.md
+++ Readme.md
@@ -1,9 +1,6 @@
 # Python client to UC3M Aula Global
-This is a modern and updated python3 fork from my friend yagop's python3 repository at https://github.com/yagop/AGClient
+This small python script allows you to download all the content from your [UC3M Aula Global](http://aulaglobal.uc3m.es) courses (.pdf, .docx, .pptx, etc.). It works as a crawler, checking the site for files as if it was you with your browser.
 
-This small python script allows you to download all the content from your [UC3M Aula Global](http://aulaglobal.uc3m.es) courses (.pdf, .docx, .pptx, etc.)
-
-To achieve user authentication, I use the Moodle API that involves making an https request that sends the student password as a parameter in the URL.
 ## Usage
 Download the repository as zip, clone it with git, or just use curl to get only the script. On GNU/Linux or MacOS:
 ```
@@ -11,16 +8,20 @@ curl -o aulaglobal.py "https://raw.githubusercontent.com/tairosonloa/Aula_Global
 python3 aulaglobal.py
 ```
 ## Requirements
-Python version 3.5 or above is required to run the script.
-Most of GNU/Linux distributions call the package "python3". To install it on Debian based GNU/Linux distros (Ubuntu, Linux Mint, ...):
+Python version 3.5 or above is required to run the script. Also, you'll need two libraries: `mechanize` and `beautifoulsoup`.
+
+Most of GNU/Linux distributions call the python package as "python3". To install dependences on Debian based GNU/Linux distros (Ubuntu, Linux Mint, ...):
 ```
-sudo apt-get install python3
+sudo apt-get install python3 python3-pip
+pip3 install --user mechanize beautifulsoup
 ```
-To install python 3 on MacOS:
+To install dependences on MacOS:
 ```
 xcode-select --install
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install python3
+python3 get-pip.py
+pip3 mechanize beautifulsoup
 ```
 ## License
 This script is under [MIT license](https://github.com/tairosonloa/Aula_Global_UC3M/blob/master/LICENSE).
--- aulaglobal.py
+++ aulaglobal.py
@@ -3,145 +3,93 @@
 # Copyright (C) 2018 Aitor Alonso at aalonso@aalonso.eu
 # This script is under MIT license
 #
-# Version: 2019.02
+# Version: 2019.10.30
 # You can find new versions and fixes of this script over the time at
 # https://github.com/tairosonloa/Aula_Global_UC3M
 
-"""
-This script allows you to download all the
-content from your UC3M Aula Global courses.
-"""
-
-import getpass
-import urllib.request
-import urllib.parse
-import json
-import xml.etree.ElementTree as elementTree
+import http.cookiejar as cookielib
+from bs4 import BeautifulSoup as bs
+import cgi
+import mechanize
 import os
-import re
-
-domain = "aulaglobal.uc3m.es"
-webservice = "/webservice/rest/server.php"
-service = "ag_mobile"
+import getpass
 
 
-# First we need the token
-def get_token(user, passwd):
-    safe_passwd = urllib.parse.quote(passwd, safe='')
-    url_token = "https://" + domain + "/login/token.php?username=" \
-        + user + "&password=" + safe_passwd + "&service=" + service
-    req = urllib.request.Request(url_token)
-    resp = urllib.request.urlopen(req).read()
+BASE_URL = "https://aulaglobal.uc3m.es"
 
-    # JSON
-    data = json.loads(resp.decode("utf8"))
-    token = data.get("token")
+# Set the browser for the web crawler
+br = mechanize.Browser()
+cookiejar = cookielib.LWPCookieJar()
+br.set_cookiejar(cookiejar)
 
-    # If token is None wrong user/passwd
-    if token is None:
-        print(data.get("error"))
-        exit()
-    return token
+br.set_handle_equiv(True)
+br.set_handle_gzip(True)
+br.set_handle_redirect(True)
+br.set_handle_referer(True)
+br.set_handle_robots(False)
 
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time = 1)
+br.addheaders = [( 'User-agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36' )]
+br.open(BASE_URL)
 
-# Get the userid necessary for get user courses
-def get_user_info(token):
-    url_info = "https://" + domain + webservice + "?wstoken=" + token \
-        + "&wsfunction=core_webservice_get_site_info"
-    req = urllib.request.Request(url_info)
-    resp = urllib.request.urlopen(req).read()
+# Ask for NIA and password
+print("##################################################################\n" +
+        "# Download all the content from your courses at UC3M Aula Global #\n" +
+        "##################################################################\n")
+user = input("Enter NIA: ")
+passwd = getpass.getpass(prompt="Enter password: ")
 
-    # XML
-    root = elementTree.fromstring(resp)
-    print("Logged as " + root.find("SINGLE/KEY[@name='fullname']/VALUE").text)
-    user_id = root.find("SINGLE/KEY[@name='userid']/VALUE").text
-    lang = root.find("SINGLE/KEY[@name='lang']/VALUE").text
-    return user_id, lang
+# Submit login form
+print("Login in...")
+br.select_form(nr=0)
+br.form['adAS_username'] = user
+br.form['adAS_password'] = passwd
+br.submit(id="submit_ok")
 
-
-# Just simply return a list of courses ids with its courses names
-def get_courses(token, user_id, lang):
-    url_courses = "https://" + domain + webservice + "?wstoken=" \
-        + token + "&wsfunction=core_enrol_get_users_courses&userid=" \
-        + user_id
-    req = urllib.request.Request(url_courses)
-    resp = urllib.request.urlopen(req).read()
-
-    root = elementTree.fromstring(resp)
-    ids = root.findall("MULTIPLE/SINGLE/KEY[@name='id']/VALUE")  # This is a list
-    names = root.findall("MULTIPLE/SINGLE/KEY[@name='fullname']/VALUE")
-    courses = [None] * len(names)
-
-    # Format names. Get the name in the correct language and remove metadata
-    for i in range(len(names)):
-        full_name = re.findall(".+?(?=[0-9]+/+[0-9]+)[0-9]+/+[0-9]+-+\w{2}",names[i].text)
-        if len(full_name) == 2:
-            if lang == "es":
-                courses[i] = {"course_id": ids[i].text, "course_name": re.sub(".[0-9]+/+[0-9]+-\w*", "", full_name[0])}
-            elif lang == "en":
-                courses[i] = {"course_id": ids[i].text, "course_name": re.sub(".[0-9]+/+[0-9]+-\w*", "", full_name[1])}
-        elif len(full_name) == 1:
-            courses[i] = {"course_id": ids[i].text, "course_name": re.sub(".[0-9]+/+[0-9]+-\w*", "", full_name[0])}
-    return courses
+# Check if success
+url = br.open(BASE_URL)
+login = url.get('X-Frame-Options', None)
+status, _ = cgi.parse_header(login)
+if status.upper() == "DENY":
+    print("Login failed. Check your NIA and password and try again")
+    exit(1)
 
 
-# Get the course contents (files urls)
-def get_course_content(token, course_id):
-    url_course = "https://" + domain + webservice + "?wstoken=" + token \
-                 + "&wsfunction=core_course_get_contents&courseid=" + course_id
+# Inspect home page for courses and save them on a set
+print("Checking for courses...")
+courses = set()
+soup = bs(url, "html.parser")
+for link in soup.findAll("a"):
+    href = link.get("href")
+    if href is not None and "/course/view.php" in href:
+        courses.add(href)
 
-    req = urllib.request.Request(url_course)
-    resp = urllib.request.urlopen(req).read()
-    root = elementTree.fromstring(resp)
-    xml_modules = "MULTIPLE/SINGLE/KEY[@name='modules']/MULTIPLE/"
-    xml_contents = "SINGLE/KEY[@name='contents']/MULTIPLE/SINGLE"
-    file_contents = root.findall(xml_modules + xml_contents)
-    files = []
-    for file_content in file_contents:
-        file_url = file_content.find("KEY[@name='fileurl']/VALUE").text
-        file_name = file_content.find("KEY[@name='filename']/VALUE").text
-        file_type = file_content.find("KEY[@name='type']/VALUE").text
-        if file_type == "file":
-            moodle_file = {"file_name": file_name, "file_url": file_url}
-            files.append(moodle_file)
-    return files
+# Check every course page
+for course in courses:
+    url = br.open(course)
+    soup = bs(url, "html.parser")
+    h1 = soup.find("h1").text
 
+    # Don't check Sala de Estudiantes nor Secretaría
+    if not "Sala de Estudiantes" in h1 and not "Secretaría" in h1:
 
-# Saves the files on disk
-def save_files(token, course_name, files):
-    if len(files) == 0:
-        print("\tThis course has not content.")
-    else:
-        path = 'courses/' + course_name  # Where the files will be saved
+        # Create folder where files will be downloaded
+        path = os.path.join("courses/", h1.replace("/","."))
+        print("\nChecking for files in " + h1)
         if not os.path.exists(path):
             os.makedirs(path)
+        
+        # Check for files to download
+        for link in soup.findAll("a"):
+            href = link.get("href")
+            if href is not None and "/mod/resource/view.php" in href:
 
-        for moodle_file in files:
-            print("\tDownloading: " + moodle_file['file_name'])
-            url = moodle_file['file_url'] + '&token=' + token
-            file = path + '/' + moodle_file['file_name']
-            response = urllib.request.urlopen(url)
-            fh = open(file, 'wb')
-            fh.write(response.read())
-            fh.close()
-
-
-def main():
-    print("##################################################################\n" +
-          "# Download all the content from your courses at UC3M Aula Global #\n" +
-          "##################################################################\n")
-    user = input("Enter NIA: ")
-    passwd = getpass.getpass(prompt="Enter password: ")
-    token = get_token(user, passwd)
-    userid, lang = get_user_info(token)
-    courses = get_courses(token, userid, lang)
-    for course in courses:
-        if course is not None:
-            print("\nCourse: " + course["course_name"])
-            files_url = get_course_content(token, course["course_id"])
-            save_files(token, course["course_name"], files_url)
-    print("\nAll files were downloaded.")
-
-
-if __name__ == '__main__':
-    main()
+                # Donwload file and get filename from response header
+                response = br.open(href)
+                cdheader = response.get('Content-Disposition', None)
+                value, params = cgi.parse_header(cdheader)
+                file  = os.path.join(path, params["filename"].encode("latin-1").decode("utf-8"))
+                print("\tDownloading: " + params["filename"].encode("latin-1").decode("utf-8"))
+                fh = open(file, 'wb')
+                fh.write(response.read())
+                fh.close()
